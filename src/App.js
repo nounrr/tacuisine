207436350155f4c81307189afee6ca3258be4dca
Med Nour <sosi66581@gmail.com>
2024-12-09 19:16:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ColorButtons from './Button';
+
+function App() {
+  return (
+    <div className="App">
+     <ColorButtons/>
+    </div>
+  );
+}
+
+
+
+export default App;
