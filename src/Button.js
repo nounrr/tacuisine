0fdec0c44cac74ab67ea6694c58f900fa26c9c24
ZAINAB ESSAHRAOUI <macintosh@MacBook-Pro-de-MacBookPro.local>
2024-12-09 19:43:26 +0100

    Message clair

--- src/Button.js
+++ src/Button.js
@@ -5,11 +5,11 @@ import Button from '@mui/material/Button';
 export default function ColorButtons() {
   return (
     <Stack direction="row" spacing={2}>
-      <Button color="secondary">Secondary</Button>
+      <Button color="primary">Secondary</Button>
       <Button variant="contained" color="success">
         Success
       </Button>
-      <Button variant="outlined" color="error">
+      <Button variant="outlined" color="light">
         Error
       </Button>
     </Stack>
