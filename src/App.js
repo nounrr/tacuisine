c125b3092b5bb0a007d5bf91d168cacc0a58ba97
Med Nour <sosi66581@gmail.com>
2025-01-25 00:43:46 +0100

    ttt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Bienvenue from './components/BienvenuPages/Bienvenue';
 // import Acceuil from './components/Acceuil/acceuil';
-import Detail from './components/Detail/Detail';
 import './assest/css/App.css';
 import PhoneNav from './components/NavBar/PhoneNav';
-<<<<<<< HEAD
 import ProfilePage from './components/ProfilePages/ProfilePage';
-=======
-import Acceuil from './components/Acceuil/acceuil';
-import UploadFile from './components/AjouterRecettes/comp/UploadFile';
-import Page1 from './components/AjouterRecettes/Pages/Page1';
-import Modal from './components/filter/drawer';
-import Detail from './components/Detail/detail';
->>>>>>> e341ee47c4eb3628ab32d45eec66c4732f11bd3b
+
 const App = () => {
     
 
     return (
-<<<<<<< HEAD
         <>
     <PhoneNav/>
 
@@ -33,16 +23,7 @@ const App = () => {
             
     </Router>
     </>
-=======
-       <><Router>
-            <Acceuil/>    
-            <Routes>
-                <Route path="/detail" element={<Detail/>} />
-            </Routes>        
-        </Router>
-        </>
-        
->>>>>>> e341ee47c4eb3628ab32d45eec66c4732f11bd3b
+      
     );
 };
 
--- src/components/NavBar/PhoneNav.jsx
+++ src/components/NavBar/PhoneNav.jsx
@@ -13,13 +13,9 @@ import { PiChefHatLight } from "react-icons/pi";
 const PhoneNav = () => {
     
 
-<<<<<<< HEAD
     return (<div className={style.menu}>
        <nav className={style.navBar}>
-=======
-    return (<>
-      <nav className={style.navBar}>
->>>>>>> e341ee47c4eb3628ab32d45eec66c4732f11bd3b
+    
           <div className={`${style.left} ${style.navDiv}`}>
           
           <div className={style.icons}>
@@ -42,12 +38,8 @@ const PhoneNav = () => {
        <PiChefHatLight className={style.icon} />
 
        </div>
-<<<<<<< HEAD
        </div>
-=======
        
-       </>
->>>>>>> e341ee47c4eb3628ab32d45eec66c4732f11bd3b
     );
 };
 
